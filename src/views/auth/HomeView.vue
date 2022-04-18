6829c89bfc7f0418ad55e84d270275e4662845cc
Juan David Zea <vikzea@gmail.com>
2022-04-18 14:35:00 -0500

    feat: home view

--- src/views/auth/HomeView.vue
+++ src/views/auth/HomeView.vue
@@ -4,6 +4,14 @@ import AuthLayout from '../layouts/AuthLayout.vue';
 
 <template>
 	<AuthLayout title="Inicio">
-		<h1>Inicio</h1>
+		<p>
+			<span class="font-bold">Capilog</span> es una herramienta web para facilitar la comunicación entre los
+			actores del proceso logistico (gestión de solicitudes de transporte) en la empresa
+			<a target="_blank" class="text-indigo-600 underline" href="https://www.capiro.co/">Capiro</a>.
+
+			<iframe class="w-full h-[700px] mt-4" src="https://www.capiro.co/blog/">
+				Fallback text for non-supported browsers
+			</iframe>
+		</p>
 	</AuthLayout>
 </template>
--- src/views/layouts/GuestLayout.vue
+++ src/views/layouts/GuestLayout.vue
@@ -1,7 +1,7 @@
 <template>
-	<div class="bg-scrap flex lg:flex-col">
+	<div class="bg-scrap h-screen flex lg:flex-col">
 		<div class="w-8 bg-gray-800 bg-opacity-90 lg:h-8 lg:w-full" />
-		<div class="min-h-screen flex-grow">
+		<div class="flex-grow">
 			<div class="flex h-full lg:flex-col">
 				<div class="flex-grow">
 					<slot />
